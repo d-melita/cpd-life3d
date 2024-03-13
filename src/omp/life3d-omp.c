a99500c2c4a59f0561a2e86adb0628792071df08
Diogo Antunes <diogo.santiago.pt@gmail.com>
2024-03-13 18:21:20 +0000

    openmp: reduced thread creation

--- src/omp/life3d-omp.c
+++ src/omp/life3d-omp.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 
 #include "world_gen.h"
 
@@ -22,6 +23,12 @@ typedef struct args {
   float density;
   int seed;
 } Args;
+
+uint32_t peak_gen[N_SPECIES + 1];
+
+uint64_t max_population[N_SPECIES + 1];
+
+uint64_t population[N_SPECIES + 1];
 
 void help() { fprintf(stderr, "\nUsage: life3d gen_count N density seed\n"); }
 
@@ -44,7 +51,7 @@ Args parse_args(int argc, char *argv[]) {
 
   args.n = atoi(argv[2]);
 
-  if (args.n == 0) {
+  if (args.n < 3) {
     printf("Invalid N provided: %s\n", argv[2]);
     help();
     exit(1);
@@ -130,18 +137,22 @@ inline char next_inhabitant(uint32_t x, uint32_t y, uint32_t z, uint32_t n,
   }
 }
 
+void prepare() {
+  memset(peak_gen, 0, sizeof(uint32_t) * (N_SPECIES + 1));
+  memset(max_population, 0, sizeof(uint64_t) * (N_SPECIES + 1));
+  memset(population, 0, sizeof(uint64_t) * (N_SPECIES + 1));
+}
+
+void finish() {
+  // fprintf(stderr, "Simulation finished\n");
+  for (uint32_t specie = 1; specie <= N_SPECIES; specie++) {
+    printf("%d %ld %d\n", specie, max_population[specie], peak_gen[specie]);
+ }
+}
+
 void simulation(uint32_t n, uint32_t max_gen, char ***grid) {
   uint32_t sim = 1;
   char ***old, ***new, ***tmp;
-
-  uint32_t peak_gen[N_SPECIES + 1];
-  memset(peak_gen, 0, sizeof(uint32_t) * (N_SPECIES + 1));
-
-  uint64_t max_population[N_SPECIES + 1];
-  memset(max_population, 0, sizeof(uint64_t) * (N_SPECIES + 1));
-
-  uint64_t population[N_SPECIES + 1];
-  memset(population, 0, sizeof(uint64_t) * (N_SPECIES + 1));
 
   old = grid;
   new = new_grid(n);
@@ -150,62 +161,60 @@ void simulation(uint32_t n, uint32_t max_gen, char ***grid) {
   // debug(n, grid);
 
   // Compute initial stats
-  #pragma omp parallel for collapse(3) reduction(+: max_population[:N_SPECIES+1]) shared(old)
-  for (uint32_t x = 0; x < n; x++) {
-    for (uint32_t y = 0; y < n; y++) {
-      for (uint32_t z = 0; z < n; z++) {
-        max_population[old[x][y][z]]++;
-      }
-    }
-  }
-
-  // fprintf(stderr, "Initial stats ================================\n");
-  // for (uint32_t specie = 1; specie <= N_SPECIES; specie++) {
-  //   fprintf(stderr, "%d %ld %d\n", specie, max_population[specie],
-  //           peak_gen[specie]);
-  // }
-
-  for (uint32_t gen = 0; gen < max_gen; gen++) {
-    memset(population, 0, sizeof(uint64_t) * (N_SPECIES + 1));
-    #pragma omp parallel for collapse(3) reduction(+: population[:N_SPECIES+1]) shared(old, new)
+  #pragma omp parallel
+  {
+    #pragma omp for collapse(3) reduction(+:population[:N_SPECIES+1])
     for (uint32_t x = 0; x < n; x++) {
       for (uint32_t y = 0; y < n; y++) {
         for (uint32_t z = 0; z < n; z++) {
-
-          new[x][y][z] = next_inhabitant(x, y, z, n, old);
-          population[new[x][y][z]]++;
-          // fprintf(stderr, "next_inhabitant(%d, %d, %d, %d, grid) = %d\n", x, y,
-          //         z, n, new[x][y][z]);
+          max_population[old[x][y][z]]++;
         }
       }
     }
 
-    tmp = old;
-    old = new;
-    new = tmp;
-
-    // fprintf(stderr, "Grid after generation %d =====================\n",
-    //         gen + 1);
-    // debug(n, old);
-    //
-    // fprintf(stderr, "Stats for generation %d ======================\n",
-    //         gen + 1);
+    // fprintf(stderr, "Initial stats ================================\n");
     // for (uint32_t specie = 1; specie <= N_SPECIES; specie++) {
-    //   fprintf(stderr, "%d %ld\n", specie, population[specie]);
+    //   fprintf(stderr, "%d %ld %d\n", specie, max_population[specie],
+    //           peak_gen[specie]);
     // }
 
-    for (uint32_t specie = 1; specie <= N_SPECIES; specie++) {
-      if (max_population[specie] < population[specie]) {
-        max_population[specie] = population[specie];
-        peak_gen[specie] = gen + 1;
+    #pragma omp single
+    {
+      memset(population, 0, sizeof(uint64_t) * (N_SPECIES + 1));
+    }
+
+    for (uint32_t gen = 0; gen < max_gen; gen++) {
+      #pragma omp for collapse(3) reduction(+:population[:N_SPECIES+1])
+      for (uint32_t x = 0; x < n; x++) {
+        for (uint32_t y = 0; y < n; y++) {
+          for (uint32_t z = 0; z < n; z++) {
+
+            new[x][y][z] = next_inhabitant(x, y, z, n, old);
+            population[new[x][y][z]]++;
+            // fprintf(stderr, "next_inhabitant(%d, %d, %d, %d, grid) = %d\n", x, y,
+            //         z, n, new[x][y][z]);
+          }
+        }
+      }
+
+      #pragma omp single
+      {
+        tmp = old;
+        old = new;
+        new = tmp;
+
+        for (uint32_t specie = 1; specie <= N_SPECIES; specie++) {
+          if (max_population[specie] < population[specie]) {
+            max_population[specie] = population[specie];
+            peak_gen[specie] = gen + 1;
+          }
+        }
+        memset(population, 0, sizeof(uint64_t) * (N_SPECIES + 1));
       }
     }
   }
 
-  // fprintf(stderr, "Simulation finished\n");
-  for (uint32_t specie = 1; specie <= N_SPECIES; specie++) {
-    printf("%d %ld %d\n", specie, max_population[specie], peak_gen[specie]);
-  }
+  
 }
 
 int main(int argc, char *argv[]) {
@@ -213,11 +222,16 @@ int main(int argc, char *argv[]) {
   Args args = parse_args(argc, argv);
   char ***grid = gen_initial_grid(args.n, args.density, args.seed);
 
+  prepare();
+
   exec_time = -omp_get_wtime();
 
   simulation(args.n, args.gen_count, grid);
 
   exec_time += omp_get_wtime();
+  
+  finish();
+
   fprintf(stderr, "Took: %.1fs\n", exec_time);
 
   return EXIT_SUCCESS;
